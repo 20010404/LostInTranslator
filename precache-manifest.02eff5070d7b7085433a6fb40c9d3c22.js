9118ae17208af665cf449bb5abbfc8bebf85aed2
20010404 <18015765247@qq.com>
2023-11-23 15:51:06 +0900

    Updates

---
+++ precache-manifest.02eff5070d7b7085433a6fb40c9d3c22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b6095016b5cd5d1b0e2fc74f0e69c7",
+    "url": "/LostInTranslator/index.html"
+  },
+  {
+    "revision": "faea29dfed51e5b78df3",
+    "url": "/LostInTranslator/static/css/2.52fe55b1.chunk.css"
+  },
+  {
+    "revision": "32833825c523cea90c71",
+    "url": "/LostInTranslator/static/css/main.9dd2bb3a.chunk.css"
+  },
+  {
+    "revision": "faea29dfed51e5b78df3",
+    "url": "/LostInTranslator/static/js/2.31c033c4.chunk.js"
+  },
+  {
+    "revision": "1de3271ccc1b1a86e7da",
+    "url": "/LostInTranslator/static/js/3.cdb5e0c1.chunk.js"
+  },
+  {
+    "revision": "b6f41eac4f7bbb015797",
+    "url": "/LostInTranslator/static/js/4.b923dd79.chunk.js"
+  },
+  {
+    "revision": "566b2c8fe1ba6d340939",
+    "url": "/LostInTranslator/static/js/5.ad6d4114.chunk.js"
+  },
+  {
+    "revision": "32833825c523cea90c71",
+    "url": "/LostInTranslator/static/js/main.fe220691.chunk.js"
+  },
+  {
+    "revision": "6198954087e5614912b3",
+    "url": "/LostInTranslator/static/js/runtime~main.6becd950.js"
+  }
+]);
